6d47f8a5ea3cf9b6feb42f223f93cb5d354b0481
mahmoud elbalhi <mahmoud1234elbalhi@gmail.com>
2024-11-13 00:04:28 +0200

    third_day

--- aprtio/src/components/LivingSpace.jsx
+++ aprtio/src/components/LivingSpace.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "../style/LivingSpace.css";
 import pic5 from "../imgs/pic5.jpg";
+import pic6 from "../imgs/image-gallery.png";
+import pic7 from "../imgs/spatula.png";
+import pic8 from "../imgs/video.png";
+import pic9 from "../imgs/cctv.png";
 export default function LivingSpace() {
   return (
     <div className="living">
@@ -14,25 +18,36 @@ export default function LivingSpace() {
 
         <div className="branches_cont">
           <div className="branch_">
-            <div className="icon"></div>
+            <div className="icon">
+              <img src={pic8} alt="" />
+            </div>
             <h2>Entertainment</h2>
             <p>Mini Home Cinema View</p>
+            <div className="line"></div>
           </div>
 
           <div className="branch_">
-            <div className="icon"></div>
+            <div className="icon">
+              <img src={pic9} alt="" />
+            </div>
             <h2>Fully Secured</h2>
             <p>24/7 With CCTV Cameras</p>
+            <div className="line"></div>
+
           </div>
 
           <div className="branch_">
-            <div className="icon"></div>
+            <div className="icon">
+              <img src={pic6} alt="" />
+            </div>
             <h2>Outside View</h2>
             <p>Garden, Deck, Roof Space...</p>
           </div>
 
           <div className="branch_">
-            <div className="icon"></div>
+            <div className="icon">
+              <img src={pic7} alt="" />
+            </div>
             <h2>Chef & Cooking</h2>
             <p>Fresh Pizza & Coffee etc</p>
           </div>
