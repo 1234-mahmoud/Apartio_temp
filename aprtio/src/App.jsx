ffb3e0a252b39358fed3f3dfed42f7f5938d4ef1
mahmoud elbalhi <mahmoud1234elbalhi@gmail.com>
2024-11-14 00:36:34 +0200

    forth day

--- aprtio/src/App.jsx
+++ aprtio/src/App.jsx
@@ -4,14 +4,32 @@ import Slider from "./components/Slider";
 import SideBar from "./components/SideBar";
 import Statistics from "./components/Statistics";
 import LivingSpace from "./components/LivingSpace";
+import Apartment from "./components/Apartment";
+import Furniture from "./components/Furniture";
 function App() {
   return (
     <div className="App">
       <Header />
       <Slider />
       <SideBar />
-      <Statistics/>
-      <LivingSpace/>
+      <Statistics />
+      <LivingSpace />
+      <Apartment
+        p1={"Apartio With Best Deals & Favours"}
+        title={"Select Apartment Style"}
+        p2={`Sed perspiciatis unde omnis iste natus error sit 
+          voluptatem laudantium totam rem
+          aperiam eaqu ipsa quae ab illo inventore
+          veritatis met quasis.`}
+      />
+      <Furniture />
+      <Apartment
+        p1={"Apartio With Best Deals & Favours"}
+        title={"Apartment Floor Plans"}
+        p2={`Sed perspiciatis unde omnis iste natus error 
+          sit voluptatem laudantium totam rem
+           aperiam eaqu ipsa quae ab illo inventore veritatis met quasis.`}
+      />
     </div>
   );
 }
